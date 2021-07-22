741a282f9b2947ef49eac3e054e86668f9b1141f
Jeffrey Belford <belfordjeffrey@gmail.com>
2021-07-22 00:43:38 -0400

    lesson 54

--- Source/BullCowGame/BullCowCartridge.cpp
+++ Source/BullCowGame/BullCowCartridge.cpp
@@ -56,39 +56,43 @@ void UBullCowCartridge::EndGame()
 
 void UBullCowCartridge::ProcessGuess(FString Guess)
 {
+
+    if(HiddenWord.Len() != Guess.Len())
+    {
+        PrintLine(TEXT("the hidden word is %i letters long"), HiddenWord.Len());
+        PrintLine(TEXT("%i Lives remaining"), Lives);
+        return;
+    }
+
     if(Guess.Equals(HiddenWord, ESearchCase::IgnoreCase))
     {
         PrintLine(TEXT("You have won!"));
         EndGame();
         return;
     }
-    else {
-        if(HiddenWord.Len() != Guess.Len())
-        {
-            PrintLine(TEXT("the hidden word is %i letters long"), HiddenWord.Len());
-            PrintLine(TEXT("%i Lives remaining"), Lives);
-            return;
-        }
-        if(!IsIsogram(Guess))
-        {
-            PrintLine(TEXT("your word was not an isogram"));
-            PrintLine(TEXT("%i Lives remaining"), Lives);
-            return;
 
-        }
-        Lives--;
-        PrintLine(TEXT("Lose a life! %i remaining"), Lives);
-        PrintLine(TEXT("Guess again"));
-        if(Lives < 1)
-        { 
-            PrintLine(TEXT("The game is over, you have Lost!"));
-            EndGame();
-            return;
-        }  
+
+    if(!IsIsogram(Guess))
+    {
+        PrintLine(TEXT("your word was not an isogram"));
+        PrintLine(TEXT("%i Lives remaining"), Lives);
+        return;
+
     }
+
+    Lives--;
+    PrintLine(TEXT("Lose a life! %i remaining"), Lives);
+    PrintLine(TEXT("Guess again"));
+    if(Lives < 1)
+    { 
+        PrintLine(TEXT("The game is over, you have Lost!"));
+        EndGame();
+        return;
+    }  
+    
 }
 
-bool UBullCowCartridge::IsIsogram(FString word)
+bool UBullCowCartridge::IsIsogram(FString word) const
 {
     for (int32 i = 0; i < word.Len()-1; i++)
     {
